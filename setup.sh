964cd992ab3f063b20e3deb6d7e6835d563a5f19
PineappleLazagna <89866971+PineappleLazagna@users.noreply.github.com>
2023-11-07 22:05:48 +0100

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+UTILS="build-essential git make neovim"
+X11="libx11-dev libxinerama-dev libxft-dev xserver-xorg xinit xinput x11-xserver-utils"
+apt install $UTILS $X11
+mkdir ./suckless && cd .suckless
+git clone https://git.suckless.org/dmenu
+git clone https://git.suckless.org/dwm
+git clone https://git.suckless.org/st
+cd dwm && make clean install
+cd ../st && make clean install
+cd ../dmenu && make clean install && cd ../..
+
